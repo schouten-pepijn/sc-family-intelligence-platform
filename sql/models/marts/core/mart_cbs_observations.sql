15a19e0cc4605d36414f0a5446a3a0b624a130bf
Pepijn Schouten <pepijn.sch@gmail.com>
2026-04-19 01:21:33 +0200

    refactor: redesign observation mart columns and add computed metrics (cbs_observations)

--- sql/models/marts/core/mart_cbs_observations.sql
+++ sql/models/marts/core/mart_cbs_observations.sql
@@ -1,13 +1,28 @@
+with staged as (
+    select *
+    from {{ ref('stg_cbs_observations') }}
+)
+
 select
     source_name,
-    natural_key,
-    retrieved_at,
     run_id,
+    retrieved_at,
+    natural_key as observation_key,
     observation_id,
-    measure_code,
-    period_code,
-    region_code,
-    numeric_value,
+    measure_code as measure_id,
+    period_code as period_id,
+    case
+        when period_code ~ '^[0-9]{4}' then cast(substring(period_code from 1 for 4) as integer)
+        else null
+    end as period_year,
+    case
+        when substring(period_code from 5 for 2) = 'JJ' then 'year'
+        when substring(period_code from 5 for 2) = 'KW' then 'quarter'
+        when substring(period_code from 5 for 2) = 'MM' then 'month'
+        else 'other'
+    end as period_granularity,
+    region_code as region_id,
+    numeric_value as observation_value,
     value_attribute,
     string_value
-from {{ ref('stg_cbs_observations') }}
+from staged
