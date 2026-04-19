4cc658fe6655bfe4fe059101b39d5ddab65aa69a
Pepijn Schouten <pepijn.sch@gmail.com>
2026-04-19 22:41:24 +0200

    refactor: expose gold writer imports in __init__ (gold.__init__)

--- src/fip/gold/__init__.py
+++ src/fip/gold/__init__.py
@@ -1,1 +1,30 @@
-"""Gold layer helpers for Postgres materialization."""
+from fip.gold.bag_pand_writer import BAGPandLandingWriter
+from fip.gold.bag_verblijfsobject_writer import BAGVerblijfsobjectLandingWriter
+from fip.gold.cbs_observations_writer import (
+    CBS_OBSERVATION_FIELDS,
+    CBSObservationLandingWriter,
+)
+from fip.gold.cbs_reference_codes_writer import (
+    REFERENCE_FIELDS,
+    CBSReferenceCodeWriter,
+    build_reference_row,
+)
+from fip.gold.core.postgres import PostgresFullRefreshWriter
+from fip.gold.core.service import PostgresSink, write_rows_to_sink
+from fip.gold.readback import connect, count_rows, sample_rows
+
+__all__ = [
+    "BAGPandLandingWriter",
+    "BAGVerblijfsobjectLandingWriter",
+    "CBS_OBSERVATION_FIELDS",
+    "CBSObservationLandingWriter",
+    "CBSReferenceCodeWriter",
+    "PostgresFullRefreshWriter",
+    "PostgresSink",
+    "REFERENCE_FIELDS",
+    "build_reference_row",
+    "connect",
+    "count_rows",
+    "sample_rows",
+    "write_rows_to_sink",
+]
