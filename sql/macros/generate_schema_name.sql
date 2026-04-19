03da6528f38989aef4dd83a9291d7532959cb422
Pepijn Schouten <pepijn.sch@gmail.com>
2026-04-19 15:59:47 +0200

    feat: add per‑model schema override macro (sql/macros/generate_schema_name.sql)

--- sql/macros/generate_schema_name.sql
+++ sql/macros/generate_schema_name.sql
@@ -1,4 +1,6 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
+    {# Allow explicit per-model schema override while defaulting to target.
+       Enables layered architecture (staging, marts) using dbt_project.yml config. #}
     {%- if custom_schema_name is none -%}
         {{ target.schema }}
     {%- else -%}
